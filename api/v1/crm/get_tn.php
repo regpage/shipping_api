aeb8791d292b408d2d1dde9e89c3211fe260a5b9
regpage <90186795+regpage@users.noreply.github.com>
2022-08-21 19:34:52 +0300

    Update get_tn.php

--- api/v1/crm/get_tn.php
+++ api/v1/crm/get_tn.php
@@ -7,7 +7,7 @@ $stage = $_GET['stage'];
 
 
 
-    $link = 'https://bibleforall.envycrm.com/crm/api/v1/deal/list/?api_key=ecdfd3e079da4ab92942a50d8dd67991b5878f21';
+    $link = 'https://bibleforall.envycrm.com/crm/api/v1/deal/list/?api_key=';
     $curl = curl_init();
 
     $data = [
